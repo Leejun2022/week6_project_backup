9dce8de95712ccc19a2165826ee1d1ffcb4b0992
jeonghyeon <kjhhy1005@gmail.com>
2022-10-22 00:14:31 +0900

    업뎃 딜릿 추가

--- services/users.service.js
+++ services/users.service.js
@@ -3,6 +3,7 @@ const UsersRepository = require("../repositories/users.repository");
 const { ValidationError } = require("../exceptions/index.exception");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
+const { nextTick } = require("process");
 
 class UsersService {
   constructor() {
@@ -10,47 +11,54 @@ class UsersService {
   }
 
   createUser = async (ID, password, confirm) => {
-    try {
-      if (password !== confirm) {
-        throw new ValidationError("비밀번호를 확인해주세요");
-      }
-      const findId = await this.usersRepository.findOneId(ID);
-      if (findId) {
-        throw new ValidationError("이미 사용중인 아이디입니다.");
-      }
-      const createUser = await this.usersRepository.createUser(ID, password);
-      return createUser;
-    } catch (error) {
-      next(error);
+    if (password !== confirm) {
+      throw new ValidationError("비밀번호를 확인해주세요");
     }
+    const findId = await this.usersRepository.findOneId(ID);
+    if (findId) {
+      throw new ValidationError("이미 사용중인 아이디입니다.");
+    }
+    const createUser = await this.usersRepository.createUser(ID, password);
+    return createUser;
   };
   loginUser = async (ID, password) => {
-    try {
-      const user = await this.usersRepository.findOneId(ID);
+    const user = await this.usersRepository.findOneId(ID);
 
-      if (!user) {
-        throw new ValidationError("아이디 또는 패스워드가 틀립니다.");
-      }
-      if (password !== user.password) {
-        throw new ValidationError("아이디 또는 패스워드가 틀립니다.");
-      }
-      const accessToken = jwt.sign(
-        { userId: user.userId },
-        process.env.SECRET_KEY,
-        { expiresIn: "1d" }
-      );
-      const refreshToken = jwt.sign(
-        { userId: user.userId },
-        process.env.SECRET_KEY,
-        { expiresIn: "21d" }
-      );
-
-      await this.usersRepository.updateRefresh(refreshToken, user);
-
-      return [user, accessToken, refreshToken];
-    } catch (error) {
-      next(error);
+    if (!user) {
+      throw new ValidationError("아이디 또는 패스워드가 틀립니다.");
     }
+    if (password !== user.password) {
+      throw new ValidationError("아이디 또는 패스워드가 틀립니다.");
+    }
+    const accessToken = jwt.sign(
+      { userId: user.userId },
+      process.env.SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+    const refreshToken = jwt.sign(
+      { userId: user.userId },
+      process.env.SECRET_KEY,
+      { expiresIn: "21d" }
+    );
+
+    await this.usersRepository.updateRefresh(refreshToken, user);
+
+    return [user, accessToken, refreshToken];
+  };
+  checkUser = async (userId) => {
+    const existUser = await this.usersRepository.findOneUser(userId);
+
+    return existUser;
+  };
+  deleUser = async (userId) => {
+    await this.userRepository.deleUser(userId);
+  };
+  updatUser = async (userId, nickname) => {
+    const existUser = await this.usersRepository.findOneNickname(nickname);
+    if (existUser) {
+      throw new ValidationError("중복된 닉네임이 있습니다.");
+    }
+    await this.userRepository.updatUser(userId, nickname);
   };
 }
-module.exports = UsersController;
+module.exports = UsersService;
