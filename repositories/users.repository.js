f002d212e8fd84270949ca8c67411b0fe1b0adf3
jeonghyeon <kjhhy1005@gmail.com>
2022-10-22 00:14:22 +0900

    레포 추가 업뎃 딜릿

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,20 +1,27 @@
 const { User } = require("../models");
 
 class UsersRepository {
-  findOneId = async (userId) => {
+  findOneUser = async (userId) => {
     const findOneId = await User.findOne({ where: { userId } });
     return findOneId;
+  };
+  findOneNickname = async (nickname) => {
+    const findOneNickname = await User.findOne({ where: { nickname } });
+    return findOneNickname;
   };
   createUser = async (ID, nickname, password) => {
     const createUser = await User.create({ ID, nickname, password });
     return createUser;
   };
-  deleteUser = async (userId, ID, password) => {
+  deleUser = async (userId, ID, password) => {
     const deleteUser = await User.destroy({ where: { userId, ID, password } });
     return deleteUser;
   };
   updateRefresh = async (refreshToken, user) => {
     await User.update({ refreshToken }, { where: { userId: user.userId } });
+  };
+  updatUser = async (userId, nickname) => {
+    await User.update({ nickname }, { where: userId });
   };
 }
 
