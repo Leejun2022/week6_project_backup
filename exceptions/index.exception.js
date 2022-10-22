349c9a22941615bb95e0dc4ce037ed5852d5caeb
jeonghyeon <kjhhy1005@gmail.com>
2022-10-22 15:45:10 +0900

    throw new error 추가

--- exceptions/index.exception.js
+++ exceptions/index.exception.js
@@ -14,5 +14,13 @@ class ValidationError extends Error {
     this.name = "ValidationError";
   }
 }
+class MiddlewareError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status || 409;
+    this.name = "MiddlewareError";
+    if (!message) this.message = "요청한 데이터 형식이 올바르지 않습니다.";
+  }
+}
 
 module.exports = { InvalidParamsError, ValidationError };
