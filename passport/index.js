2e098fcf17ebc8da9e1543d8b9a912fba1fc7e3c
jeonghyeon <kjhhy1005@gmail.com>
2022-10-24 12:25:19 +0900

    머지전 한번 체크

--- passport/index.js
+++ passport/index.js
@@ -6,12 +6,12 @@ const User = require("../models/user");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.userId);
   });
 
-  passport.deserializeUser((id, done) => {
+  passport.deserializeUser((userId, done) => {
     //? 두번 inner 조인해서 나를 팔로우하는 followerid와 내가 팔로우 하는 followingid를 가져와 테이블을 붙인다
-    User.findOne({ where: { id } })
+    User.findOne({ where: { userId } })
       .then((user) => done(null, user))
       .catch((err) => done(err));
   });
