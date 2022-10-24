6033b722fae73bcb352976a9deb14f7b75005f2b
leejun <koreadlwns@naver.com>
2022-10-24 15:13:23 +0900

    posts commit

--- migrations/20220731133318-create-posts.js
+++ migrations/20220731133318-create-posts.js
@@ -1,37 +1,41 @@
-"use strict";
-/** @type {import('sequelize-cli').Migration} */
+'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Comments", {
-      commentId: {
+    await queryInterface.createTable('Posts', {
+      postId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
       },
-      postId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      comment: {
+      title: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      content: {
+        type: Sequelize.STRING
+      },
       nickname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
         type: Sequelize.STRING,
         allowNull: false,
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-      },
+        defaultValue: Sequelize.fn('now')
+      }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Comments");
-  },
+    await queryInterface.dropTable('Posts');
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const commentRouter = require("./comments.routes");
-const recommentRouter = require("./recomments.routes");
-
-// const pageRouter = require("./routes/page");
-const authRouter = require("./auth");
-
-const userRouter = require("./users.routes");
-
-router.use("/comments", commentRouter);
-router.use("/recomments/comments", recommentRouter);
-
-router.use("/users", [userRouter]);
-// router.use("/", pageRouter);
-router.use("/auth", authRouter);
-
+const postsRouter = require('./posts.routes');
+router.use('/posts/', postsRouter);
 
 module.exports = router;
