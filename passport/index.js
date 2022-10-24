110bf2ba188902f67f57303721c92cfd6dcb9708
jeonghyeon <kjhhy1005@gmail.com>
2022-10-24 12:27:29 +0900

    일단체크

--- passport/index.js
+++ passport/index.js
@@ -4,17 +4,15 @@ const kakao = require("./kakaoStrategy"); // 카카오서버로 로그인할때
 
 const User = require("../models/user");
 
+//로그인 시에만 실행
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    done(null, user.userId);
+    done(null, user);
   });
-
-  passport.deserializeUser((userId, done) => {
-    //? 두번 inner 조인해서 나를 팔로우하는 followerid와 내가 팔로우 하는 followingid를 가져와 테이블을 붙인다
-    User.findOne({ where: { userId } })
-      .then((user) => done(null, user))
-      .catch((err) => done(err));
+  //매 요청시 실행
+  passport.deserializeUser((user, done) => {
+    done(null, user);
   });
-
-  kakao(); // 구글 전략 등록
+  kakao();
+  google();
 };
